ebf3bdce766f264bbfc3ab4ab977ed2876324eb0
Filipe Santos Correa <filipe.santos.correa@ergodirekt.de>
2019-04-02 17:56:05 +0200

    Add styles for tablet and desktop devices

--- src/components/about.js
+++ src/components/about.js
@@ -5,8 +5,8 @@ export default () => (
   <>
     <h1 className={styles.headline}>Filipe Santos Correa</h1>
     <p className={styles.underline}>
-      I'm a Frontend Engineer. <br/>
-      I love to create new things everyday.
+      I'm a <strong>Frontend Engineer</strong>.<br/>
+      I <strong>love</strong> to create new things everyday.
     </p>
   </>
 )
