429197f9a38f5b3fd475307fe7a4d56ff04c5da2
Filipe Santos Correa <filipe.santos.correa@ergodirekt.de>
2019-04-02 18:05:31 +0200

    Configure particles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import Particles from 'react-particles-js';
 import styles from './index.module.css'
 import About from '../components/about'
 import Links from '../components/links'
+import particleOptions from '../../assets/particles.json'
 
 export default () => (
-  <div className={styles.outerContainer}>
-    <div className={styles.container}>
-      <About />
-      <Links />
+  <>
+    <Particles className={styles.particles} params={particleOptions} />
+    <div className={styles.outerContainer}>
+      <div className={styles.container}>
+        <About />
+        <Links />
+      </div>
     </div>
-  </div>
+  </>
 )
