7893108d5e92df8c0a7dec4e45870c12f001fcd5
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-04-07 15:12:41 +0200

    Fix wording in about.js

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ export default () => (
   <>
     <h1 className={styles.headline}>Filipe Santos Correa</h1>
     <p className={styles.underline}>
-      I'm a <strong>Frontend Engineer</strong>.<br/>
+      I'm a <strong>Front-End</strong> Engineer.<br/>
       I <strong>love</strong> to create new things everyday.
     </p>
   </>
