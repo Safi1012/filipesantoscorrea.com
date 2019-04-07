f039afb83d9f9199350f2e344cb0e40022f336eb
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-04-07 12:00:34 +0200

    Improve accessibility

--- src/components/links.js
+++ src/components/links.js
@@ -10,19 +10,19 @@ import Mail from "../../assets/icons/mail.svg";
 export default () => (
   <div className={styles.links}>
 
-    <OutboundLink href="https://github.com/Safi1012" rel="noopener noreferrer" target="_blank">
+    <OutboundLink href="https://github.com/Safi1012" rel="noopener noreferrer" target="_blank" aria-label="Link to my GitHub profile">
       <GitHub alt="GitHub icon" />
     </OutboundLink>
 
-    <OutboundLink href="https://shrinkme.app" rel="noopener noreferrer" target="_blank">
+    <OutboundLink href="https://shrinkme.app" rel="noopener noreferrer" target="_blank" aria-label="Link to my side project named Shrink Me">
       <ShrinkMe alt="Shrink Me icon" />
     </OutboundLink>
 
-    <OutboundLink href="https://shrinkme.app" rel="noopener noreferrer" target="_blank">
+    <OutboundLink href="https://www.linkedin.com/in/filipe-santoscorrea" rel="noopener noreferrer" target="_blank" aria-label="Link to my LinkedIn profile">
       <LinkedIn alt="LinkedIn icon" />
     </OutboundLink>
     
-    <OutboundLink href="mailto:mail@filipesantoscorrea.com" rel="noopener noreferrer" target="_blank">
+    <OutboundLink href="mailto:mail@filipesantoscorrea.com" rel="noopener noreferrer" target="_blank" aria-label="Link to my email address">
       <Mail alt="Mail icon" />
     </OutboundLink>
     
