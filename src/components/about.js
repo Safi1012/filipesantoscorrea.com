d17024021f9d14ee792c3b2e646fbe9e1fe2e72a
Hamed Esmaili <mr.hesmaili@gmail.com>
2020-03-18 02:09:06 +0330

    chore: update links

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ export default () => (
     <span>Hi <span role="img" aria-label="hi">👋</span> I am</span>
     <h1 className={styles.headline}>Hamed Esmaili</h1>
     <p className={styles.underline}>
-      I'm a <strong>Front-End</strong> Engineer.<br/>
+      I'm a <strong>Software</strong> Engineer.<br/>
       I <strong>love</strong> to create new things in hopes of being able to help others.
     </p>
   </>
--- src/components/links.js
+++ src/components/links.js
@@ -2,29 +2,35 @@ import React from 'react'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
 
 import styles from './links.module.css'
+import Twitter from "../../assets/icons/twitter.svg";
 import GitHub from "../../assets/icons/github.svg";
-import ShrinkMe from "../../assets/icons/shrink-me.svg";
 import LinkedIn from "../../assets/icons/linkedIn.svg";
-import Mail from "../../assets/icons/mail.svg";
+import Instagram from "../../assets/icons/instagram.svg";
+import Medium from "../../assets/icons/medium.svg";
+import Codepen from "../../assets/icons/codepen.svg";
 
 export default () => (
-  <div className={styles.links}>
-
-    <OutboundLink href="https://github.com/Safi1012" rel="noopener noreferrer" target="_blank" aria-label="Link to my GitHub profile">
-      <GitHub alt="GitHub icon" />
-    </OutboundLink>
-
-    <OutboundLink href="https://shrinkme.app" rel="noopener noreferrer" target="_blank" aria-label="Link to my side project named Shrink Me">
-      <ShrinkMe alt="Shrink Me icon" />
-    </OutboundLink>
-
-    <OutboundLink href="https://www.linkedin.com/in/filipe-santoscorrea" rel="noopener noreferrer" target="_blank" aria-label="Link to my LinkedIn profile">
-      <LinkedIn alt="LinkedIn icon" />
-    </OutboundLink>
-    
-    <OutboundLink href="mailto:mail@filipesantoscorrea.com" rel="noopener noreferrer" aria-label="Link to my email address">
-      <Mail alt="Mail icon" />
-    </OutboundLink>
-    
-  </div>
+  <>
+    <div className={styles.links}>
+      <OutboundLink href="https://twitter.com/theham3d" rel="noopener noreferrer" target="_blank" aria-label="Link to my Twitter profile">
+        <Twitter alt="Twitter icon" />
+      </OutboundLink>
+      <OutboundLink href="https://github.com/theham3d" rel="noopener noreferrer" target="_blank" aria-label="Link to my GitHub profile">
+        <GitHub alt="GitHub icon" />
+      </OutboundLink>
+      <OutboundLink href="https://www.linkedin.com/in/theham3d" rel="noopener noreferrer" target="_blank" aria-label="Link to my LinkedIn profile">
+        <LinkedIn alt="LinkedIn icon" />
+      </OutboundLink>
+      <OutboundLink href="https://www.instagram.com/theham3d/" rel="noopener noreferrer" target="_blank" aria-label="Link to my Instagram profile">
+        <Instagram alt="Instagram icon" />
+      </OutboundLink>
+      <OutboundLink href="https://medium.com/@theham3d" rel="noopener noreferrer" target="_blank" aria-label="Link to my Medium profile">
+        <Medium alt="Medium icon" />
+      </OutboundLink>
+      <OutboundLink href="https://codepen.io/theham3d" rel="noopener noreferrer" target="_blank" aria-label="Link to my Codepen profile">
+        <Codepen alt="Codepen icon" />
+      </OutboundLink>
+    </div>
+    <span className={styles.email}>{`{ theham3d@gmail.com }`}</span>
+  </>
 )
