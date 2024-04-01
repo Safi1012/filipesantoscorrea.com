58c1914c1d0869c458441de51dfa6c0e91a7b9df
Filipe Santos Correa <mail@filipesantoscorrea.com>
2024-04-01 17:40:52 +0200

    Add deployment to pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  })
-});
+export default defineConfig({});
